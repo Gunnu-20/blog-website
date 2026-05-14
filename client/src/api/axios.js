b9124bd84000f8e6e353ad8c369cd94093441a7b
Gunnu <gungunrathore200406@gmail.com>
2026-05-14 09:35:42 +0530

    LIVE URL-ADD

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://blog-website-backend-g1it.onrender.com" });
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem("blogUser"));
