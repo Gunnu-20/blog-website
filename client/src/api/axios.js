8c10c29da921cd80cd476a4f5da255801d9175f5
Gunnu <gungunrathore200406@gmail.com>
2026-05-14 09:43:15 +0530

    error-fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://blog-website-backend.onrender.com/api" });
+const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem("blogUser"));
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "https://blog-website-frontend.onrender.com", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
